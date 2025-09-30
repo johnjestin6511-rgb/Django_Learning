4e9933a3f40d20b6bcb536056ce46d6837119c96
John Jestin S <johnjestin6511@gmail.com>
2025-09-30 22:40:21 +0530

    moving firstproject to Django_LEVEL_TWO

--- firstproject/first_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from first_app import views
-
-urlpatterns =[
-    path('',views.index,name='index'),
-    path('first_app/',views.index,name='index'),
-    path('User/',views.user, name='user'),
-]
